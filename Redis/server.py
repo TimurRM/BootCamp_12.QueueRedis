a5c0921a3679285c8a727a9be4d1b1d358ce96ff
Timur <tumyp.paxumob@gmail.com>
2022-08-13 21:30:04 +0500

    Added Files

---
+++ Redis/server.py
@@ -0,0 +1,12 @@
+import redis
+import random
+
+with redis.Redis() as redis_server:
+    redis_server.lpush("queue", random.randint(1, 10))
+
+
+
+
+# redis_server = redis.Redis()
+
+# redis_server.close()
